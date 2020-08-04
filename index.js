dfbe937d669e75f6412422bec675553e01405d6f
Jeremy Rose <nornagon@nornagon.net>
2020-08-04 15:03:32 -0700

    feat: also download symbols for non-win32 platforms

--- index.js
+++ index.js
@@ -49,11 +49,6 @@ const download = (options) => {
 const SYMBOL_BASE_URL = 'https://symbols.mozilla.org'
 
 const findSymbols = async ({version, platform, arch, directory, file, quiet}) => {
-  if (platform !== 'win32') {
-    // Mozilla's symbol server doesn't have symbols for non-Windows platforms
-    return
-  }
-
   // Extract a list of referenced modules using minidump_dump
   const modules = []
   {
@@ -65,7 +60,10 @@ const findSymbols = async ({version, platform, arch, directory, file, quiet}) =>
     // minidump_dump's output has lines like:
     //   (debug_file)                    = "user32.pdb"
     //   (debug_identifier)              = "034AFFE8331738A54EC07A7655CAF0DC1"
-    const re = /\(debug_file\)\s+= "([^"]+\.pdb)"\s+\(debug_identifier\)\s+= "([0-9A-F]+)"/mg
+    // or on a linux dump:
+    //   (debug_file)                    = "/XXXX/XXXXXX/XXXX/XXX/XXXXXXXXXXXXXXXX/libc.so.6"
+    //   (debug_identifier)              = "4B76CFD3972F3EACFE366DDD07AD902F0"
+    const re = /\(debug_file\)\s+= "(?:[X/]+\/)?([^"]+)"\s+\(debug_identifier\)\s+= "([0-9A-F]+)"/mg
     let m;
     while (m = re.exec(r)) {
       const [, file, id] = m
@@ -77,9 +75,10 @@ const findSymbols = async ({version, platform, arch, directory, file, quiet}) =>
   const promises = []
   for (const [pdb, id] of modules) {
     if (pdb === 'electron.exe.pdb') continue
-    const symbolPath = path.join(directory, 'breakpad_symbols', pdb, id, pdb.replace('.pdb', '.sym'))
+    const symbolFileName = pdb.replace(/(\.pdb)?$/, '.sym')
+    const symbolPath = path.join(directory, 'breakpad_symbols', pdb, id, symbolFileName)
     if (!fs.existsSync(symbolPath) && !fs.existsSync(path.dirname(symbolPath))) {
-      const url = `${SYMBOL_BASE_URL}/${pdb}/${id}/${pdb.replace('.pdb', '.sym')}`
+      const url = `${SYMBOL_BASE_URL}/${pdb}/${id}/${symbolFileName}`
       promises.push(new Promise((resolve, reject) => {
         // We use curl here in order to avoid having to deal with redirects +
         // gzip + saving to a file ourselves. It would be more portable to
