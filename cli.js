f00e32d50917572ac72246df0e7c5702b19896fc
Jeremy Rose <nornagon@nornagon.net>
2022-01-19 09:39:57 -0800

    path to dmp is positional, instead of with -f

--- cli.js
+++ cli.js
@@ -3,19 +3,24 @@
 const {minidump} = require('./index')
 
 const argv = require('yargs')
-  .usage('$0 [args]')
   .version(false)
-  .option('file', {
-    alias: 'f',
-    describe: 'path to minidump (.dmp) file',
-    demandOption: true,
-  })
-  .option('quiet', {
-    alias: 'q',
-    describe: 'suppress download progress output',
-  })
-  .option('force', {
-    describe: 'redownload symbols if present in cache',
+  .command('$0 <file>', 'symbolicate a textual crash dump', (yargs) => {
+    return yargs
+      .positional('file', {
+        describe: 'path to crash dump',
+      })
+      .option('f', {
+        type: 'boolean',
+        describe: 'obsolete, provided for backwards compatibility',
+      })
+      .option('quiet', {
+        alias: 'q',
+        describe: 'suppress download progress output',
+      })
+      .option('force', {
+        describe: 'redownload symbols if present in cache',
+        type: 'boolean'
+      })
   })
   .help()
   .argv
